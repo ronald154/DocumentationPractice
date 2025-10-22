5d2bc5c940c0cee072def1a1bc620f9bbf05ad34
ronal154 <ronald44@webmail.umm.ac.id>
2025-10-22 17:30:23 +0700

    add aritmatika.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
+void main() {
 
-        System.out.println("Hello, Git and Java!");
-        int a = 5;
-        int b = 3;
-        int hasil = a + b;
-
+    IO.println("Hello, Git and Java!");
+    int a = 5;
+    int b = 3;
+    int hasil = a + b;
 
 
-        System.out.println("Hasil dari " + a + " + " + b + " adalah " + hasil);
-    }
+    IO.println("Hasil dari " + a + " + " + b + " adalah " + hasil);
 }
