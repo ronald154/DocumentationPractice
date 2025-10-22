fbce3117063c7b7d326cc6e79a8cbde6cea88e1b
ronal154 <ronald44@webmail.umm.ac.id>
2025-10-22 17:30:39 +0700

    add aritmatika.java

--- src/aritmatika.java
+++ src/aritmatika.java
@@ -1,13 +1,11 @@
-public class aritmatika {
-    static void main() {
-        int a = 6;
-        int b = 3;
-        int tambah = a + b;
-        int kurang = a - b;
-        int bagi = a / b;
+void main() {
+    int a = 6;
+    int b = 3;
+    int tambah = a + b;
+    int kurang = a - b;
+    int bagi = a / b;
 
-        System.out.println("Hasil dari tambah " + a + " + " + b + " adalah " + tambah);
-        System.out.println("Hasil dari kurang " + a + " - " + b + " adalah " + kurang);
-        System.out.println("Hasil dari bagi " + a + " / " + b + " adalah " + bagi);
-    }
+    IO.println("Hasil dari tambah " + a + " + " + b + " adalah " + tambah);
+    IO.println("Hasil dari kurang " + a + " - " + b + " adalah " + kurang);
+    IO.println("Hasil dari bagi " + a + " / " + b + " adalah " + bagi);
 }
