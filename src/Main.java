8eb31460463b7bd0cac4e44a700ce188b2434006
ronal154 <ronald44@webmail.umm.ac.id>
2025-10-22 17:27:47 +0700

    add Main.java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Git and Java!");
+        int a = 5;
+        int b = 3;
+        int hasil = a + b;
+
+
+
+        System.out.println("Hasil dari " + a + " + " + b + " adalah " + hasil);
+    }
+}
